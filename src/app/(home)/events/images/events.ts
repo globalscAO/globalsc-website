e4d02420edd578cd54de4cbe2b20a9f7f50850c9
Joel Silva <joelpitra44@gmail.com>
2025-07-22 17:04:49 +0100

    feat: atualiza URLs das imagens de eventos e adiciona função para transformar texto em links

--- src/app/(home)/events/images/events.ts
+++ src/app/(home)/events/images/events.ts
@@ -1799,17 +1799,303 @@ Agradecemos, de forma especial, ao Governo da Província de Luanda, pela conside
   },
   {
     id: 16,
-    imgSrc: "https://conteudolocal.globalsc.ao/plenaria/2.JPG",
+    imgSrc:
+      "https://global-services-corporation.github.io/images/conteudo-local-jul-2025/24.png",
     mediaSrc: [
       {
         id: 1,
         type: "image",
-        src: "https://conteudolocal.globalsc.ao/plenaria/1.JPG",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/1.png",
       },
       {
         id: 2,
         type: "image",
-        src: "https://conteudolocal.globalsc.ao/plenaria/2.JPG",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/2.png",
+      },
+      {
+        id: 3,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/3.png",
+      },
+      {
+        id: 4,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/4.png",
+      },
+      {
+        id: 5,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/5.png",
+      },
+      {
+        id: 6,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/6.png",
+      },
+      {
+        id: 7,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/7.png",
+      },
+      {
+        id: 8,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/8.png",
+      },
+      {
+        id: 9,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/9.png",
+      },
+      {
+        id: 10,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/10.png",
+      },
+      {
+        id: 11,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/11.png",
+      },
+      {
+        id: 12,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/12.png",
+      },
+      {
+        id: 13,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/13.png",
+      },
+      {
+        id: 14,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/14.png",
+      },
+      {
+        id: 15,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/15.png",
+      },
+      {
+        id: 16,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/16.png",
+      },
+      {
+        id: 17,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/17.png",
+      },
+      {
+        id: 20,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/20.png",
+      },
+      {
+        id: 21,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/21.png",
+      },
+      {
+        id: 22,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/22.png",
+      },
+      {
+        id: 23,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/23.png",
+      },
+      {
+        id: 24,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/24.png",
+      },
+      {
+        id: 25,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/25.png",
+      },
+      {
+        id: 26,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/26.png",
+      },
+      {
+        id: 27,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/27.png",
+      },
+      {
+        id: 28,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/28.png",
+      },
+      {
+        id: 29,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/29.png",
+      },
+      {
+        id: 30,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/30.png",
+      },
+      {
+        id: 31,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/31.png",
+      },
+      {
+        id: 32,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/32.png",
+      },
+      {
+        id: 33,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/33.png",
+      },
+      {
+        id: 34,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/34.png",
+      },
+      {
+        id: 35,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/35.png",
+      },
+      {
+        id: 36,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/36.png",
+      },
+      {
+        id: 37,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/37.png",
+      },
+      {
+        id: 38,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/38.png",
+      },
+      {
+        id: 39,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/39.png",
+      },
+      {
+        id: 40,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/40.png",
+      },
+      {
+        id: 41,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/41.png",
+      },
+      {
+        id: 42,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/42.png",
+      },
+      {
+        id: 44,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/44.png",
+      },
+      {
+        id: 45,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/45.png",
+      },
+      {
+        id: 46,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/46.png",
+      },
+      {
+        id: 48,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/48.png",
+      },
+      {
+        id: 50,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/50.png",
+      },
+      {
+        id: 51,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/51.png",
+      },
+      {
+        id: 52,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/52.png",
+      },
+      {
+        id: 53,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/53.png",
+      },
+      {
+        id: 54,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/54.png",
+      },
+      {
+        id: 55,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/55.png",
+      },
+      {
+        id: 56,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/56.png",
+      },
+      {
+        id: 57,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/57.png",
+      },
+      {
+        id: 58,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/58.png",
+      },
+      {
+        id: 59,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/59.png",
+      },
+      {
+        id: 60,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/60.png",
+      },
+      {
+        id: 62,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/62.png",
+      },
+      {
+        id: 63,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/63.png",
+      },
+      {
+        id: 70,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/70.png",
+      },
+      {
+        id: 71,
+        type: "image",
+        src: "https://global-services-corporation.github.io/images/plenaria/conteudo-local-jul-2025/71.png",
       },
     ],
     title: `FÓRUM SOBRE: O IMPACTO DO SECTOR FINANCEIRO NO CONTEÚDO LOCAL`,
